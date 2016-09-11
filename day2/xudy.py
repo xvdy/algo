9f21db79d269dbe262b025e2c09f13d8e7ae7ad3
xudongyang <xudongyang@chunyu.me>
2016-09-11 14:55:14 +0800

    day2 day3代码补全

--- day2/xudy.py
+++ day2/xudy.py
@@ -1,1 +1,13 @@
-print "todo next"
+def permute(num):
+    if len(num) == 0: return []
+    if len(num) == 1: return [num]
+    res = []
+    for i in range(len(num)):
+        for j in permute(num[:i] + num[i + 1:]):
+            res.append([num[i]] + j)
+    return res
+
+
+r = permute([1, 2, 3])
+print(r)
+
--- day3/xudy.py
+++ day3/xudy.py
@@ -1,1 +1,18 @@
-print "todo next"
+def f(s):
+    tmp = s[0]
+    for i in xrange(1, len(s)):
+        if tmp[-1] == s[i]:
+            tmp += s[i]
+        else:
+            tmp = s[i]
+        if len(tmp) == 3:
+            s = s[:i - 2] + s[i + 1:]
+            return f(s)
+    return s
+
+
+r = f("12233324")
+s = f("AABBBABBBA")
+
+print(r)
+print(s)
