3baf0ac90cb921a7caf5441598873e785ff75dd6
xudongyang <xudongyang@chunyu.me>
2016-09-30 23:32:44 +0800

    finish day41: anagrams.

--- day41/xudy.py
+++ day41/xudy.py
@@ -1,1 +1,20 @@
-print "todo next"
+# -*- coding:utf-8 -*-
+
+tests = ["tea", "and", "ate", "eat", "den"]
+
+
+def anagrams(strs):
+    sorted_strs = [''.join(sorted(str_)) for str_ in strs]
+    result = []
+    for index, item in enumerate(sorted_strs):
+        same_items = [sorted_str for sorted_str in sorted_strs if sorted_str == item]
+        if len(same_items) > 2:
+            result.append(strs[index])
+
+    return result
+
+
+if __name__ == '__main__':
+    results = anagrams(tests)
+
+    print(results)
