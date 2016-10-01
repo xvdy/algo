c62eaea4a9b01da5999d960bab12bc3959c6fd98
xudongyang <xudongyang@chunyu.me>
2016-10-01 10:51:15 +0800

    finish day39: roman 2 int.

--- day39/xudy.py
+++ day39/xudy.py
@@ -1,1 +1,27 @@
-print "todo next"
+# -*- coding:utf-8 -*-
+
+
+def roman2int(roman):
+    char_map = {'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000}
+
+    sum = char_map[roman[-1]]
+    index = len(roman) - 2
+    while index >= 0:
+        if char_map[roman[index]] < char_map[roman[index + 1]]:
+            sum -= char_map[roman[index]]
+        else:
+            sum += char_map[roman[index]]
+        index -= 1
+
+    return sum
+
+
+if __name__ == '__main__':
+    result = roman2int("VIII")
+    print(result)
