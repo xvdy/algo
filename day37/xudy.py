226772aa2ce9d37935316e6ed5206328ddff46df
xudongyang <xudongyang@chunyu.me>
2016-10-01 11:11:38 +0800

    finish day37: is valid num.

--- day37/xudy.py
+++ day37/xudy.py
@@ -1,1 +1,13 @@
-print "todo next"
+# -*- coding:utf-8 -*-
+import re
+
+
+def is_valid_num(str):
+    num_pattern = '^[+-]?\d+(\.\d+)?(e[+-]?\d+(\.\d+)?)?$'
+
+    return True if re.match(num_pattern, str) else False
+
+
+if __name__ == '__main__':
+    result = is_valid_num('1.1e1')
+    print(result)
