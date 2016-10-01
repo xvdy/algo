82eec84249f79f5052d446fe2c291a6902df7c15
xudongyang <xudongyang@chunyu.me>
2016-10-01 11:25:20 +0800

    finish day36: get longest common prefix.

--- day36/xudy.py
+++ day36/xudy.py
@@ -1,1 +1,24 @@
-print "todo next"
+# -*- coding:utf-8 -*-
+
+
+def get_longest_common_prefix(l):
+    def get_common_prefix(a, b):
+        if not a or not b:
+            return ''
+        steps = min(len(a), len(b))
+        result = ''
+        index = 0
+        while index < steps:
+            if a[index] == b[index]:
+                result += a[index]
+            else:
+                return result
+            index += 1
+        return result
+
+    return reduce(get_common_prefix, l)
+
+
+if __name__ == '__main__':
+    result = get_longest_common_prefix(["abcdef", "abcdk", "abcfk"])
+    print(result)
