a57e2503f07295a39ad9c5b7bcbc1d6057ea27b3
xudongyang <xudongyang@chunyu.me>
2016-10-07 23:27:27 +0800

    finish day42: path parse.

--- day42/xudy.py
+++ day42/xudy.py
@@ -1,1 +1,22 @@
-print "todo next"
+# -*- coding:utf-8 -*-
+
+
+def parse_path(path):
+    split_path = path.split('/')
+    result_path = []
+    for path_item in split_path:
+        if not path_item or path_item == '.':
+            continue
+        elif path_item == '..':
+            if result_path:
+                result_path.pop()
+        else:
+            result_path.append(path_item)
+
+    return '/' + '/'.join(result_path)
+
+if __name__ == '__main__':
+    tests = ['//', '/home/', '/a/./b/../../c/', '/../']
+    for test in tests:
+        result = parse_path(test)
+        print(result)
