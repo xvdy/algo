c3dd9f45441d0572d55c178cad73a1d6b6f47f07
xudongyang <xudongyang@chunyu.me>
2016-10-08 14:07:41 +0800

    finish day47: 逆波兰表达式.

--- day47/xudy.py
+++ day47/xudy.py
@@ -1,1 +1,25 @@
-print "todo next"
+# -*- coding:utf-8 -*-
+
+
+def get_reverse_polish_result(expression):
+    stack = []
+    ops = {'+': lambda x, y: x+y,
+           '-': lambda x, y: x-y,
+           '*': lambda x, y: x*y,
+           '/': lambda x, y: x/y,
+           }
+    for item in expression:
+        if item in ['+', '-', '*', '/']:
+            right = stack.pop()
+            left = stack.pop()
+            stack.append(ops.get(item)(left, right))
+        else:
+            stack.append(int(item))
+
+    return stack.pop()
+
+if __name__ == '__main__':
+    tests = [['2', '1', '+', '3', '*'], ['4', '13', '5', '/', '+']]
+    for test in tests:
+        result = get_reverse_polish_result(test)
+        print(result)
