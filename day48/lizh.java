213a37614d4372631ec13ea8254efa27467e8dea
lizhongaho <lizhonghao@ihep.ac.cn>
2016-10-09 00:01:58 +0800

    day48

--- day48/lizh.java
+++ day48/lizh.java
@@ -1,4 +1,4 @@
-// todo next “lizhonghao-2016-10-07”
+// todo next “lizhonghao-2016-10-09”
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -19,6 +19,15 @@ public class PreorderTraversal {
 		root.right.right=null;
 		System.out.println("非递归方法先序遍历的结果:"+preOrderTranversal(root));
 		System.out.println("递归方法先序遍历的结果:"+preOrderTranversal_recursive(root));
+		System.out.println();
+		
+		System.out.println("非递归方法中序遍历的结果:"+inOrderTranversal(root));
+		System.out.println("递归方法中序遍历的结果:"+inOrderTranversal_1(root));
+		System.out.println();
+		
+		System.out.println("非递归方法后序遍历的结果:"+postOrderTranversal(root));
+		System.out.println("递归方法后序遍历的结果:"+postOrderTranversal_1(root));
+		System.out.println();
 	}
 	
 	public static class TreeNode{
@@ -73,6 +82,108 @@ public class PreorderTraversal {
 		result.add(root.val);
 		recursive(root.left, result);
 		recursive(root.right, result);
+	}
+	
+	//中序遍历
+	//非递归
+	public static ArrayList<Integer> inOrderTranversal(TreeNode root){
+		ArrayList<Integer> result=new ArrayList<>();
+		Stack<TreeNode> stack=new Stack<>();
+		
+		TreeNode cur=root;
+		while(cur!=null || !stack.empty()){
+			while(cur!=null){
+				stack.push(cur);
+				cur=cur.left;
+			}
+			
+			cur=stack.peek();
+			stack.pop();
+			result.add(cur.val);
+			cur=cur.right;
+		}
+		
+		return result;
+	}
+	
+	//递归实现
+	public static ArrayList<Integer> inOrderTranversal_1(TreeNode root){
+		ArrayList<Integer> result=new ArrayList<>();
+		
+		if(root!=null){
+			recursive_1(root,result);
+		}
+		
+		return result;
+	}
+	
+	public static void recursive_1(TreeNode p,ArrayList<Integer> result){
+		if(p.left!=null){
+			recursive_1(p.left,result);
+		}
+		
+		result.add(p.val);
+		
+		if(p.right!=null){
+			recursive_1(p.right,result);
+		}
+	}
+	
+	//后序遍历
+	//非递归;这个方法非常巧妙！！
+	public static ArrayList<Integer> postOrderTranversal(TreeNode root){
+		ArrayList<Integer> result=new ArrayList<>();
+		Stack<TreeNode> stack=new Stack<>();
+		
+		if(root==null){
+			return result;
+		}
+		
+		stack.push(root);
+		while(!stack.empty()){
+			TreeNode temp=stack.peek();
+			if(temp.left==null && temp.right==null){
+				result.add(stack.pop().val);
+			}else{
+				if(temp.right!=null){
+					stack.push(temp.right);
+					temp.right=null;
+				}
+				
+				if(temp.left!=null){
+					stack.push(temp.left);
+					temp.left=null;
+				}
+			}
+		}
+		
+		return result;	
+	}
+	
+	//递归实现
+	public static ArrayList<Integer> postOrderTranversal_1(TreeNode root){
+		ArrayList<Integer> result=new ArrayList<>();
+		
+		if(root==null){
+			return result;
+		}
+		
+		recursive_2(root, result);
+		
+		return result;
+	}
+	
+	public static void recursive_2(TreeNode root,ArrayList<Integer> result){
+		if(root==null){
+			return;
+		}
+		
+		recursive_2(root.left, result);
+		//System.out.println(result);
+		
+		recursive_2(root.right,result);
+		System.out.println(result);
+		//result.add(root.val);
 	}
 }
 
