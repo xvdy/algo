597fe22b8ea7418208724e23951411c3e07d9bd5
lizhongaho <lizhonghao@ihep.ac.cn>
2016-10-09 22:24:20 +0800

    day49

--- day49/lizh.java
+++ day49/lizh.java
@@ -15,7 +15,7 @@ public class LevelOrderTraversal {
 		root.left=new TreeNode(9);
 		root.right=new TreeNode(20);
 		root.right.left=new TreeNode(15);
-		root.right.left=new TreeNode(7);
+		root.right.right=new TreeNode(7);
 		
 		System.out.println("层序遍历的结果为："+levelOrderTraversal(root));
 	}
