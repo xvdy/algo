a437ea51cae6b5888a93730c25a9dc69398d6861
xudongyang <xudongyang@chunyu.me>
2016-10-18 21:02:59 +0800

    finish day60: unique binary tree count.

--- day60/xudy.py
+++ day60/xudy.py
@@ -1,1 +1,19 @@
-print "todo next"
+# -*- coding:utf-8 -*-
+
+
+def number_binary_tree(n):
+    count = [1, 1]
+
+    for i in xrange(2, n + 1):
+        current = 0
+        for j in xrange(i):
+            current += count[j] * count[i - j - 1]
+        count.append(current)
+
+    return count[n]
+
+
+if __name__ == '__main__':
+    result = number_binary_tree(3)
+    assert result==5
+
