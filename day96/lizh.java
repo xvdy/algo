3cfa4b65fba38b886dad8df9dfecc3d4bab00aa2
lizhongaho <lizhonghao@ihep.ac.cn>
2016-11-09 23:17:29 +0800

    day96

--- day96/lizh.java
+++ day96/lizh.java
@@ -1,3 +1,35 @@
-// todo next “lizhonghao-2016-09-7”
+// todo next “lizhonghao-2016-11-09”
+import java.util.ArrayList;
 
+public class GenerateParentheses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println(generateParentheses(3));
+	}
+	
+	public static void helper(ArrayList<String> result,String str,int left,int right){
+		if(left==0 && right==0){
+			result.add(str);
+		}
+		
+		if(left>0){
+			helper(result, str+"(", left-1, right);
+		}
+		
+		if(right>0 && left<right){
+			helper(result, str+")", left, right-1);
+		}
+	}
+	
+	public static ArrayList<String> generateParentheses(int n){
+		ArrayList<String> result=new ArrayList<>();
+		if(n<=0){
+			return result;
+		}
+		
+		helper(result, "", n, n);
+		return result;
+	}
+}
 
